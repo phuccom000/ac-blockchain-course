b521c7314930f5651e1294eddfe1809eeeb1f8ea
phuccom000 <gdtitans6@gmail.com>
2026-09-24 22:15:08 +0700

    Create Profile.sol

---
+++ lessons/bai3_1/Profile.sol
@@ -0,0 +1,11 @@
+pragma solidity ^0.8.0;
+
+contract Profile {
+    string public name;
+    uint public age;
+
+    function setProfile(string memory _name, uint _age) public {
+        name = _name;
+        age = _age;
+    }
+}
